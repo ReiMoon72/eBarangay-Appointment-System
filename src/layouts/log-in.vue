44e2571b898aad61aff79d04c3571b3c8b30f14a
ReiMoon72 <rhelpalma6@gmail.com>
2026-05-27 11:36:26 +0800

    Sign In/Log Out Okey na

--- src/layouts/log-in.vue
+++ src/layouts/log-in.vue
@@ -8,11 +8,11 @@ const LogoIMG = ref(FAQImage);
 
 <template>
   <section class="font-inter p-5">
-    <div class=" flex items-center justify-center">
+    <div class="flex items-center justify-center">
       <div
-        class="bg-gray-400 bg-blend-multiply bg-[url(/img/Bgimage.png)] h-120 w-130 md:h-120 bg-cover bg-center rounded-l-sm"
+        class="bg-gray-400 bg-blend-multiply hidden md:bg-[url(/img/Bgimage.png)] h-120 w-130 md:h-120 bg-cover bg-center rounded-l-sm flex flex-col flex-wrap md:flex-row"
       >
-        <div class="flex items-center">
+        <div class="flex items-center text-white">
           <img class="w-25" :src="LogoIMG" />
           <div>
             <b class="text-2xl"> eBarangay</b>
@@ -20,34 +20,39 @@ const LogoIMG = ref(FAQImage);
           </div>
         </div>
       </div>
-      <form @click.prevent class=" font-inter flex flex-col gap-3 items-baseline justify-center bg-amber-50 text-black w-130 h-120 rounded-r-sm p-8">
-        <div class=" text-blue-950">
-            <p class=" text-2xl"><b>Sign in to your account</b></p>
-            <p>Enter Your Credential To Enter Your Account</p>
+      <form
+        @click.prevent
+        class="font-inter flex flex-col gap-3 items-baseline justify-center bg-amber-50 text-black w-90 md:w-130 h-120 rounded-r-sm p-8 flex-col md:flex-col"
+      >
+        <div class="text-blue-950">
+          <p class="text-2xl"><b>Sign in to your account</b></p>
+          <p>Enter Your Credential To Enter Your Account</p>
         </div>
-        <label>Email Address: </label>
-        <input
-          class="border-2 p-1 rounded-sm border-grayewan w-100"
-          type="text"
-          v-model="emailAdress"
-        />
-        <label>Password: </label>
-        <input
-          class="border-2 p-1 rounded-sm border-graybg w-100"
-          type="password"
-          v-model="passwordEmail"
-        />
+        <div class="flex flex-col gap-3">
+          <label>Email Address: </label>
+          <input
+            class="border-2 p-1 rounded-sm border-grayewan w-80 md:w-100"
+            type="text"
+            v-model="emailAdress"
+          />
+          <label>Password: </label>
+          <input
+            class="border-2 p-1 rounded-sm border-graybg w-80 md:w-100"
+            type="password"
+            v-model="passwordEmail"
+          />
+        </div>
         <div>
           <button class="cursor-pointer text-blue-700">Forget Password</button>
         </div>
         <button
-          class="flex items-center justify-center gap-3 rounded-sm cursor-pointer bg-blue-900 p-1.5 text-white w-90 m-auto"
+          class="flex items-center justify-center gap-3 rounded-sm cursor-pointer bg-blue-900 p-1.5 text-white w-80 md:w-90 m-auto"
         >
           <Lock size="20" /> Sign In
         </button>
-        <div class=" flex gap-5 items-center">
-            <p>Don't have an account?</p> 
-            <button class="cursor-pointer text-blue-600">Sign In</button>
+        <div class="flex gap-5 items-center justify-center m-auto">
+          <p>Don't have an account?</p>
+          <button class="cursor-pointer text-blue-600">Sign In</button>
         </div>
       </form>
     </div>
--- src/layouts/signin.vue
+++ src/layouts/signin.vue
@@ -1,16 +1,114 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+import LogoIMG from "../assets/img/Logo-web.png";
+import { Calendar, ScanFace, MessageCircle, User } from "@lucide/vue";
 
-
+//For Logo
+const Logo = ref(LogoIMG);
 </script>
 
 <template>
-
-    <header>
-        <div>
-            <p>Signin</p>
+  <section class="font-inter p-5">
+    <div class="flex items-center justify-center">
+      <div
+        class="bg-gray-400 bg-blend-multiply hidden bg-[url(/img/cloud-bg.png)] h-120 w-130 md:h-155 bg-cover bg-center rounded-l-sm"
+      >
+        <div class="flex items-center text-white">
+          <img class="w-25" :src="Logo" />
+          <div>
+            <b class="text-2xl"> eBarangay</b>
+            <p>Appointment System</p>
+          </div>
         </div>
-    </header>
-
-
+        <div class="text-white p-5">
+          <p>
+            <b class="text-2xl">Create Your Account</b> <br />
+            Sign up now to get started with your <br />
+            Appointment and Request.
+          </p>
+        </div>
+        <div class="text-white p-3 flex flex-col gap-15">
+          <div class="flex items-center">
+            <Calendar class="p-1.5 rounded-md" size="50" color="blue" />
+            <p>
+              <b>Easy Online Appointment</b> <br />Book Goverment Services
+              <br />
+              in just a few clicks.
+            </p>
+          </div>
+          <div class="flex items-center">
+            <ScanFace class="p-1.5 rounded-md" size="50" color="blue" />
+            <p>
+              <b>Easy Online Appointment</b> <br />Book Goverment Services
+              <br />
+              in just a few clicks.
+            </p>
+          </div>
+          <div class="flex items-center gap-2">
+            <MessageCircle class="p-1.5 rounded-md" size="50" color="blue" />
+            <p>
+              <b>Easy Online Appointment</b> <br />Book Goverment Services
+              <br />
+              in just a few clicks.
+            </p>
+          </div>
+        </div>
+      </div>
+      <form
+        @click.prevent
+        class="font-inter flex flex-col gap-2 items-center justify-center bg-amber-50 text-black w-90 md:w-130 h-160 md:h-155 rounded-r-sm p-2 md:p-8"
+      >
+        <div class="text-blue-950">
+          <p class="text-2xl"><b>Create an account</b></p>
+          <p>Fill the details to create your account</p>
+        </div>
+        <label>Full Name: </label>
+        <input
+          class="border-2 p-1 rounded-sm border-grayewan w-85 md:w-100"
+          type="text"
+          v-model="emailAdress"
+        />
+        <label>Email Address: </label>
+        <input
+          class="border-2 p-1 rounded-sm border-grayewan w-85 md:w-100"
+          type="text"
+          v-model="emailAdress"
+        />
+        <label>Phone Number: </label>
+        <input
+          class="border-2 p-1 rounded-sm border-graybg w-85 md:w-100"
+          type="password"
+          v-model="passwordEmail"
+        />
+        <label>Password: </label>
+        <input
+          class="border-2 p-1 rounded-sm border-graybg w-85 md:w-100"
+          type="password"
+          v-model="passwordEmail"
+        />
+        <label>Confirm Password: </label>
+        <input
+          class="border-2 p-1 rounded-sm border-graybg w-85 md:w-100"
+          type="password"
+          v-model="Confirmpassword"
+        />
+        <div class="flex flex-col items-baseline gap-2">
+          <div>
+            <input type="checkbox" /> I agree to the
+            <span class="text-blue-700">Term of Use</span> and
+            <span class="text-blue-700">Privacy Policy</span>
+          </div>
+          <button
+            class="bg-blue-800 flex items-center p-1.5 w-85 md:w-100 justify-center text-white cursor-pointer"
+          >
+            <User /> Sign Up
+          </button>
+        </div>
+        <div class=" flex items-center m-auto gap-6">
+            <p>Already Have a Account?</p>
+            <button class=" text-blue-800" @click="Back">Sign In</button>
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
