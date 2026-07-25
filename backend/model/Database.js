00a8514e94a57c70649d9ffee32c41a9527bc721
ReiMoon72 <209888263+ReiMoon72@users.noreply.github.com>
2026-07-25 18:05:38 +0800

    Ready For Deployment

--- backend/model/Database.js
+++ backend/model/Database.js
@@ -7,7 +7,7 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
-  port: Number(process.env.DB_PORT),
+  port: 1433,
   database: process.env.DB_DATABASE,
 
   options: {
